a66f42d820228ad63c016cc49b46d875f50b73e7
dghollingsworth <dghollingsworth@gmail.com>
2014-05-29 15:08:26 -0400

    added routes for comments. Nested in pictures

--- db/migrate/20140529185158_create_comments.rb
+++ db/migrate/20140529185158_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-
+    	t.string "content"
+    	t.belongs_to "user"
+    	t.belongs_to "picture"
       t.timestamps
     end
   end
