1ad735f6b409639ea411f893332d4a79e60e31c2
dghollingsworth <dghollingsworth@gmail.com>
2014-05-28 22:21:08 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dogbook::Application.config.secret_key_base = '8f2786168cef01081aa267bc8f3c9f40c80d98c0549099a65fb717da53583439ae38fc26c21c3efd477ceec499fe5c51791d16bddc74bd16dfccbaf25c3969f2'
